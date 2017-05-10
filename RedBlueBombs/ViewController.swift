48d02985435169b99d78fc2d4e3a66635cdebc40
Shaisan2 <shivam0sharma@icloud.com>
2017-05-10 19:52:38 -0400

    Created the RedBlueBombs app

--- RedBlueBombs/ViewController.swift
+++ RedBlueBombs/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var blueBomb: UIImageView!
+    @IBOutlet weak var redBomb: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,25 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func hideBlueBomb(_ sender: UIButton) {
+        if (!blueBomb.isHidden) {
+            blueBomb.isHidden = true
+            sender.setTitle("Show Blue Bomb", for: UIControlState.normal)
+        } else {
+            blueBomb.isHidden = false
+            sender.setTitle("Hide Blue Bomb", for: UIControlState.normal)
+        }
+        
+    }
+    
+    @IBAction func hideRedBomb(_ sender: UIButton) {
+        if (!redBomb.isHidden) {
+            redBomb.isHidden = true
+            sender.setTitle("Show Red Button", for: UIControlState.normal)
+        } else {
+            redBomb.isHidden = false
+            sender.setTitle("Hide Red Button", for: UIControlState.normal)
+        }
+    }
 }
 
